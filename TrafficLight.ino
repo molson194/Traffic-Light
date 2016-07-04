f8706d0a6e9e1fe0272a94767ce5e428c904500a
Matthew Olson <molson194@gmail.com>
2016-07-03 22:14:18 -0600

    Arduino script

---
+++ TrafficLight.ino
@@ -0,0 +1,27 @@
+int red = 13;
+int yellow = 12;
+int green = 11;
+
+void setup() {
+  pinMode(red,OUTPUT);
+  pinMode(yellow,OUTPUT);
+  pinMode(green,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(green,HIGH);
+  digitalWrite(yellow,LOW);
+  delay(3000);
+
+  digitalWrite(green,LOW);
+  digitalWrite(yellow,HIGH);
+  delay(3000);
+
+  digitalWrite(yellow,LOW);
+  digitalWrite(red,HIGH);
+  delay(3000);
+
+  digitalWrite(red,LOW);
+  digitalWrite(green,HIGH);
+  delay(3000);
+}
